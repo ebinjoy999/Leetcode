bbfd45c9f431108f984159e0b8446051c9e76ea9
Ebin Joy <ebinjoy999@gmail.com>
2024-10-18 06:24:20 +0530

    Anagram

---
+++ Leetcode75/kotlin/Random Questions/GroupAnagrams.kt
@@ -0,0 +1,43 @@
+https://leetcode.com/problems/group-anagrams
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+
+
+----------------- Using HashMap
+
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+
+        val hash: HashMap<String, ArrayList<String>> = hashMapOf()
+        for (str in strs) {
+
+            val ar = str.toCharArray()
+            ar.sort()
+            val key = String(ar).  //val key = ar.joinToString()
+            if (!hash.contains(key)) {
+                hash[key] = arrayListOf()
+            } 
+
+            val items = hash[key]!!
+            items.add(str)
+            hash[key] = items
+        }
+        return ArrayList(hash.values)
+    }
+}
+
+
+
+fun main() {
+    val ar = "abs".toCharArray()
+    val key = String(ar)
+    val key2 = ar.toString()
+
+    println("key $key -- key2 $key2") 
+}
+prints
+key abs -- key2 [C@27bc2616
+
+
+----------------- 
--- Leetcode75/kotlin/Random Questions/ValidAnagram.kt
+++ Leetcode75/kotlin/Random Questions/ValidAnagram.kt
@@ -13,26 +13,89 @@ Example 2:
 Input: s = "rat", t = "car"
 Output: false
 
-
-
+------------------ Using HashMap
     class Solution {
         fun isAnagram(s: String, t: String): Boolean {
 
-            val map: HashMap<Char, Int> = hashMapOf()
+            if(s.length != t.length) return false
 
-            for (c in s.toCharArray()) {
-                map[c] = map.getOrDefault(c, 0) + 1
+            val hash: HashMap<Char, Int> = hashMapOf()
+            for (c in s) {
+                hash[c] = hash.getOrDefault(c, 0) + 1
             }
 
-            for (c in t.toCharArray()) {
-                val count = map.getOrDefault(c, 0)
-                if (count == 1) {
-                    map.remove(c)
+            for (c in t) {
+                val freq = hash.getOrDefault(c, 0)
+                if (freq==1) {
+                    hash.remove(c)
                 } else {
-                    map[c] = count-1
+                    hash[c] = freq - 1
                 }
             }
 
-            return map.isEmpty()
+            return hash.isEmpty()
         }
     }
+
+
+------------  Using Sort
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if(s.length != t.length) return false
+        val sa = s.toCharArray()
+        sa.sort()
+        val ta = t.toCharArray()
+        ta.sort()
+        for (i in sa.indices) {
+            if (sa[i] != ta[i])
+                return false
+        }
+        return true
+    }
+}
+
+
+------------  Using Selection Sort
+//worst complexity
+
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if(s.length != t.length) return false
+        val sa = sortArraySelectionSort(s.toCharArray())
+        val ta = sortArraySelectionSort(t.toCharArray())
+        for (i in sa.indices) {
+            if (sa[i] != ta[i])
+                return false
+        }
+        return true
+    }
+
+    fun sortArraySelectionSort(a: CharArray): CharArray {
+
+        var i = 0
+
+        while (i < a.size) {
+
+            var (md, j) = i to i+1
+            while (j < a.size) {
+                if (a[md] > a[j]) {
+                    md = j
+                }
+                j++
+            }
+
+            if (md != i) {
+                val t = a[md]
+                a[md] = a[i]
+                a[i] = t
+            }
+            i++
+        }
+        return a
+    }
+}
+
+
+
+
+
